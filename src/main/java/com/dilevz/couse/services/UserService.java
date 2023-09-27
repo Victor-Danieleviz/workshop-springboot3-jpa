f1c171e12fd56f51b413b4a936bd6d2fe19f8207
Victor <victordanieleviz@gmail.com>
2023-09-27 09:43:28 -0300

    Service layer, component registration

---
+++ src/main/java/com/dilevz/couse/services/UserService.java
@@ -0,0 +1,26 @@
+package com.dilevz.couse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dilevz.couse.entities.User;
+import com.dilevz.couse.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
